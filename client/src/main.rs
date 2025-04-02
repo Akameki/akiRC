c83abd5390f99adab624efcb68b22bb636c48b3c
Akameki <jwang905@umd.edu>
2025-04-02 01:00:26 -0400

    barebones CLI client

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,5 +1,33 @@
-// use std::net::TcpStream;
+use std::{io::{stdin, BufRead, BufReader, Write}, net::TcpStream};
+use owo_colors::{self, OwoColorize};
 
+// CLI program that sends and receives text to a TcpStream.
 fn main() {
-    println!("i am client!");
+    let addr = "irc.libera.chat:6667";
+    let mut stream = TcpStream::connect(addr).unwrap();
+    println!("=====\n{}", "Connected".green().italic());
+    let stream_clone = stream.try_clone().unwrap();
+    std::thread::spawn(move || {
+        let mut buf_reader = BufReader::new(stream_clone);
+        let mut buffer = String::new();
+        loop {
+            match buf_reader.read_line(&mut buffer).unwrap() {
+                0 => break, // EOF
+                _ => println!("{}", buffer.bright_blue()),
+            }
+            buffer.clear();
+        }
+        println!("\n{}=====", "Disconnected".red().italic());
+        std::process::exit(1);
+    });
+
+    let mut input = String::new();
+    loop {
+        match stdin().read_line(&mut input).unwrap() {
+            0 => break, // EOF
+            _ => stream.write_all(input.as_bytes()).unwrap(),
+        }
+        input.clear();
+    }
+
 }
